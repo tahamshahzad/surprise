5a183a6f4c19a56a15a54e207849b2d87af5ebd9
tahamshahzad <taha.shahzad.0728@gmail.com>
2016-09-04 08:22:02 +0300

    good version

--- dist/typingJs.js
+++ dist/typingJs.js
@@ -1,111 +1,104 @@
 var typeName = "typeEffect";
 
-window[typeName] = {
-    ele: null ,
-    startingPoint: 0,
-    dataLen: null ,
-    data: null ,
-    typingSpeed: null ,
-    holdingTime: null ,
-    dataAtm: null ,
-    cursor : null,
-    eleParent : null,
+function TypedJs () {
+    var ele= null ;
+    var startingPoint= 0;
+    var dataLen= null ;
+    var data= null ;
+    var typingSpeed= null ;
+    var holdingTime= null ;
+    var dataAtm= null ;
+    var showCursor = null;
+    var eleParent = null;
+    var repeat = null;
 
-    run: function(options) {
-
-        this.ele = document.querySelector(".typedContent");
-        this.eleParent = document.querySelector("#" + options.id);
-        this.data = options.data;
-        this.dataLen = this.data.length;
-        this.typingSpeed = options.typingSpeed;
-        this.holdingTime = options.holdingTime;
-        this.writingTime = options.writingTime;
-        this.repeat = options.repeat;
-        this.cursor.show();
-        this.typify();
-    },
-    typify: function() {
-
-        this.dataAtm = this.data[this.startingPoint];
-        this.createWritingEffect();
-    },
-    createWritingEffect: function() {
-        this.cursor.removeBlink();
-        var dataAtm = this.dataAtm;
+    this.run = function(options) {
+        ele = document.querySelector(".typedContent");
+        data = options.data;
+        dataLen = data.length;
+        typingSpeed = options.typingSpeed;
+        holdingTime = options.holdingTime;
+        writingTime = options.writingTime;
+        repeat = options.repeat;
+        //showCursor();
+        typify();
+    }
+    function typify() {
+        dataAtm = data[startingPoint];
+        createWritingEffect();
+    }
+    function createWritingEffect () {
+        //cursor.removeBlink();
         var dataLen = dataAtm.length;
         var endHere = 0;
         var timer = setInterval(function() {
-            window[typeName].ele.innerHTML += dataAtm[endHere];
+            ele.innerHTML += dataAtm[endHere];
             endHere++;
             if (endHere == dataLen) {
                 clearInterval(timer);
-                window[typeName].cursor.addBlink();
-                window[typeName].checkStatus();
+                //cursor.addBlink();
+                checkStatus();
             }
-        }, this.typingSpeed);
-    },
-    hold: function(callingFunc) {
+        }, typingSpeed);
+    }
+    function hold(callingFunc) {
         var time;
         if (callingFunc == "typify") {
-            time = this.writingTime;
+            time = writingTime;
+            setTimeout(function() {
+                typify();
+            }, time);
          }
          else {
-           time = this.holdingTime;
+           time = holdingTime;
+           setTimeout(function() {
+               clearWritten();
+           }, time);
          }
-        setTimeout(function() {
-            window[typeName][callingFunc]();
-        }, time);
-    },
-    clearWritten: function() {
-        this.cursor.removeBlink();
-        var dataAtm = this.dataAtm;
+
+    }
+    function clearWritten() {
+        //this.cursor.removeBlink();
         var dataLen = dataAtm.length;
         var dataToSlice = dataAtm;
         var timer = setInterval(function() {
             dataToSlice = dataToSlice.slice(0, -1);
-            window[typeName].ele.innerHTML = dataToSlice;
+            ele.innerHTML = dataToSlice;
             dataLen--;
             if (dataLen == 0) {
                 clearInterval(timer);
-                window[typeName].cursor.addBlink();
-                window[typeName].hold("typify");
+              //  window[typeName].cursor.addBlink();
+                hold("typify");
             }
         }, 60);
-    },
-    checkStatus: function() {
-        if (this.repeat == true) {
-            if (this.startingPoint == this.dataLen - 1) {
-                this.startingPoint = 0;
+    }
+     function checkStatus() {
+        if (repeat == true) {
+            if (startingPoint == dataLen - 1) {
+                startingPoint = 0;
             } else {
-                this.startingPoint++;
+                startingPoint++;
             }
 
-            this.hold("clearWritten");
-        } else if (this.repeat == false) {
-            if (this.startingPoint != this.dataLen - 1) {
-                this.startingPoint++;
-                this.hold("clearWritten");
+            hold("clearWritten");
+        } else if (repeat == false) {
+            if (startingPoint != dataLen - 1) {
+              startingPoint++;
+              hold("clearWritten");
             }
         }
-    },
-    cursor : {
-      elementHTML : '<span class="blinkingCursor blink">O</span>',
-      element : null,
-      show : function () {
-        if(window[typeName].cursor != false) {
-          window[typeName].eleParent.insertAdjacentHTML("beforeend",this.elementHTML);
-          this.element = document.querySelector(".blinkingCursor");
+    }
+      function showCursor() {
+        if(showCursor != false) {
+          ele.classList.add("blink");
         }
-
-      },
-      addBlink : function (){
-        this.element.classList.add("blink");
-      },
-      removeBlink : function() {
-        this.element.classList.remove("blink");
+      }
+      function blinkCursor(){
+        ele.classList.add("blink");
+      }
+      function stableCursor() {
+        ele.classList.add("stableCursor");
       }
 
 
-    },
-
-}
+    }
